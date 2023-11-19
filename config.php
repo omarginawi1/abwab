4390dc6a0506493c1d4a40c8c0cec1ba30552232
omar <66207435+omarginawi1@users.noreply.github.com>
2023-11-19 10:03:47 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.playtubescript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | PlayTube - The Ultimate Video Sharing Platform
+// | Copyright (c) 2017 PlayTube. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "";
+// MySQL Database User
+$sql_db_user = "u288274812_abwab";
+// MySQL Database Password
+$sql_db_pass = "123456qwertyO@";
+// MySQL Database Name
+$sql_db_name = "u288274812_abwab";
+
+// Site URL
+$site_url = "https://www.abwabgo.com"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "ccf77216-ecbf-4e5f-912c-2ba67ae29140"; // Your purchase code, don't give it to anyone.
+
+$auto_redirect = true;
+?>
